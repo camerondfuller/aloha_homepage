c461a1be7b9212a05b7b787243f326ef0668989c
Cameron Fuller <camerondfuller@gmail.com>
2016-02-05 18:33:19 +0000

    initial commit.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+
+   console.log("Hey there handsome! You're killing it today!");
+// slider
+   $('.bxslider').bxSlider({
+      controls:false,
+      minSlides:1,
+      maxSlides:4,
+      slideMargin: 0,
+      auto: true,
+      mode: 'horizontal'
+   });
+// pop-up alert for email
+   $('.sign-up-form').on('submit', function(event) {
+
+      event.preventDefault();
+
+      var userEmail = $('input[name="new-email"]');
+
+      if(userEmail.val().length !== 0 ) {
+         alert('Thanks for subscribing!');
+      } else {
+         alert('Please enter a valid email address.')
+      }
+   });
+
+
+
+
+});
